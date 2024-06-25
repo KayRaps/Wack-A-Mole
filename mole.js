b76f3a45ed5ec8c17503731a814c027bb1f264d4
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-25 11:00:03 +0200

    Added game board setup function to initialize tiles on page load.

---
+++ mole.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    setGame ();
+}
+
+function setGame() {
+    // grid for game board
+    for (let i = 0; i < 9; i++) { // i goes from 0 to 8, stops at 9
+        //<div id="0-8"></div>
+        let tile = document.createElement("div");
+        tile.id = i.toString();
+        document.getElementById("board").appendChild(tile);
+    }
+}
